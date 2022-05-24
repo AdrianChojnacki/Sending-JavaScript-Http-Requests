bd6d0c2c3f2ea06b6f2abe5ca31ea703da191c5d
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-24 11:08:14 +0200

    added sendData function with API data

--- xhr.js
+++ xhr.js
@@ -1,19 +1,40 @@
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
 
-const getData = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://reqres.in/api/users');
+const sendHttpRequest = (method, url, data) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
 
-  xhr.onload = () => {
-    const data = JSON.parse(xhr.response);
-    console.log(data);
-  };
+    xhr.responseType = "json";
 
-  xhr.send();
+    if (data) {
+      xhr.setRequestHeader("Content-Type", "application/json");
+    }
+
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
 };
 
-const sendData = () => {};
+const getData = () => {
+  sendHttpRequest("GET", "https://reqres.in/api/users").then(responseData => {
+    console.log(responseData);
+  });
+};
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+const sendData = () => {
+  sendHttpRequest("POST", "https://reqres.in/api/register", {
+    email: "eve.holt@reqres.in",
+    password: "pistol",
+  }).then(responseData => {
+    console.log(responseData);
+  });
+};
+
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", sendData);
